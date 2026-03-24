ecea9bbe4c92bf01e21623b87740d0a1ce8cd796
Hasanul Islam Sujon <68911980+sujon7603@users.noreply.github.com>
2026-03-24 11:26:52 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+const {app, BrowserWindow, ipcMain} = require('electron')
+
+app.disableHardwareAcceleration();
+
+const path = require('node:path');
+const fs = require('node:fs');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1100,
+        height: 800,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true,
+            nodeIntegration: false
+        }
+    });
+    win.loadFile('index.html');
+}
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+
+ipcMain.handle('save-note', async (event, text) => {
+    const filePath = path.join(app.getPath('documents'), 'quicknote.txt');
+    fs.writeFileSync(filePath, text, 'utf-8');
+    return { success: true};
+});
+
+ipcMain.handle('load-note', async () => {
+    const filePath = path.join(app.getPath('documents'), 'quicknote.txt');
+    if (fs.existsSync(filePath)){
+        return fs.readFileSync(filePath, 'utf-8');
+    }
+    return '';
+});
+
+ipcMain.handle('delete-note', async() => {
+    const filePath = path.join(app.getPath('documents'), 'quicknote.txt');
+    if(fs.existsSync(filePath)){
+        fs.unlinkSync(filePath);
+    }
+    return { success: true };
+});
